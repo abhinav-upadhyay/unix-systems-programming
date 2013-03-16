d302d070b928d09cb2c0b5b1efd8ec2782c3eab6
Abhinav Upadhyay <er.abhinav.upadhyay@gmail.com>
2013-03-16 14:17:15 +0530

    Add log.h for implementing loglib.

---
+++ chapter01/log.h
@@ -0,0 +1,14 @@
+#ifndef LOG_H
+#define LOG_H
+
+#include <time.h>
+
+typedef struct data_struct {
+    time_t time;
+    char *string;
+} data_t;
+
+int addmsg(data_t data);
+void clearlog(void);
+char *getlog(void);
+int savelog(char *filename);
